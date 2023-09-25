f344d348fa6ff39a096c921b1a527a46b10d3c2c
subham prasad <subham.prasad@ecosmob.com>
2023-09-25 21:23:21 +0530

    final commit

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -9,13 +9,13 @@ const NavBar = () =>
 
         <div className="grid grid-cols-7 w-90 h-[10vh] items-center px-8 border-sm shadow-sm">
             <div className="logo col-span-1">
-                Aucion
+                Auction
             </div>
             <div className="grid col-span-3 grid-cols-5 items-center h-100 justify-center text-center gap-0">
                 <NavLink
                     to="/"
                     className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "active" : ""
+                        isPending ? "pending" : isActive ? "active border-b-2 border-slate-500 py-3" : ""
                     }
                 >
                     Home
@@ -23,15 +23,15 @@ const NavBar = () =>
                 <NavLink
                     to="/products"
                     className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "active" : ""
+                        isPending ? "pending" : isActive ? "active border-b-2 border-slate-500 py-3" : ""
                     }
                 >
-                    Products
+                    Bids
                 </NavLink>
                 <NavLink
                     to="/addbids"
                     className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "active" : ""
+                        isPending ? "pending" : isActive ? "active border-b-2 border-slate-500 py-3" : ""
                     }
                 >
                     Add Bids
@@ -39,19 +39,12 @@ const NavBar = () =>
                 <NavLink
                     to="/mylisting"
                     className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "active" : ""
+                        isPending ? "pending" : isActive ? "active border-b-2 border-slate-500 py-3" : ""
                     }
                 >
                     My Listing
                 </NavLink>
-                <NavLink
-                    to="/contact"
-                    className={({ isActive, isPending }) =>
-                        isPending ? "pending" : isActive ? "active" : ""
-                    }
-                >
-                    Contact
-                </NavLink>
+              
 
             </div>
             <div className="logo col-span-3 justify-center text-end px-8">
